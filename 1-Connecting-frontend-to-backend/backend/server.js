7a0543718b384f1e8c76958cfeba4e10ba69c301
Saniya-Ali <saniya.20cse395@rtu.ac.in>
2024-05-23 07:30:49 +0530

    backend ocmplete

--- 1-Connecting-frontend-to-backend/backend/server.js
+++ 1-Connecting-frontend-to-backend/backend/server.js
@@ -1,14 +1,18 @@
+// CORS error - Cross origin resource sharing. In this it does not allow sharing from a diff origin or IP or even different port. To solve this we have to whitelist that IP or url or we have to add proxy in our app which we did.
+
 import express from 'express'
 
 const app = express();
 
 //We don't want to serve anything on '/' right now
-// app.get('/', (req, res) => {
-//     res.send('Server is ready')
-// })
+app.get('/', (req, res) => {
+    res.send('Server is ready')
+})
 
 //get a list of 5 jokes
- app.get('/jokes', (req, res)=>{
+
+//std parctice - /api/*
+ app.get('/api/jokes', (req, res)=>{
     const jokes = [
         {
             id:1,
